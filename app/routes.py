de3722bbed40a8f039f7ebd712eef46c6d2cbc7d
kamistretta <kathleen.mistretta@bobcats.gcsu.edu>
2024-10-09 11:12:02 -0400

    git

--- app/routes.py
+++ app/routes.py
@@ -5,22 +5,25 @@ from . import app
 def index():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/book_info')
+def book_info():
+    return render_template('book_info.html')
 
-@app.route('/contacts')
-def contacts():
-    return render_template('contacts.html')
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 @app.route('/events')
 def events():
     return render_template('events.html')
 
-@app.route('/runners')
-def runners():
-    return render_template('runners.html')
+@app.route('/story')
+def story():
+    return render_template('story.html')
 
-@app.route('/book_info')
-def book_info():
-    return render_template('book_info.html')
+@app.route('/upcoming')
+def upcoming():
+    return render_template('upcoming.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
