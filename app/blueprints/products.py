c65550c0f6cb9319608506b55942376d10614079
kamistretta <kathleen.mistretta@bobcats.gcsu.edu>
2024-10-09 13:22:53 -0400

    git

--- app/blueprints/products.py
+++ app/blueprints/products.py
@@ -16,7 +16,7 @@ def products_list():
         stock = request.form['stock']
 
         # Insert the new product info into the database
-        cursor.execute('INSERT INTO product_info (product_name, category, price, stock) VALUES (%s, %s, %s, %s)',
+        cursor.execute('INSERT INTO products (product_name, category, price, stock) VALUES (%s, %s, %s, %s)',
                        (product_name, category, price, stock))
         db.commit()
 
@@ -24,7 +24,7 @@ def products_list():
         return redirect(url_for('products.products_list'))
 
     # Handle GET request to display all products
-    cursor.execute('SELECT * FROM product_info')
+    cursor.execute('SELECT * FROM products')
     all_products = cursor.fetchall()
     return render_template('product_info.html', all_products=all_products)
 
@@ -40,7 +40,7 @@ def update_product(product_id):
         price = request.form['price']
         stock = request.form['stock']
 
-        cursor.execute('UPDATE product_info SET product_name = %s, category = %s, price = %s, stock = %s WHERE product_id = %s',
+        cursor.execute('UPDATE products SET product_name = %s, category = %s, price = %s, stock = %s WHERE product_id = %s',
                        (product_name, category, price, stock, product_id))
         db.commit()
 
@@ -48,7 +48,7 @@ def update_product(product_id):
         return redirect(url_for('products.products_list'))
 
     # GET method: fetch product's current data for pre-populating the form
-    cursor.execute('SELECT * FROM product_info WHERE product_id = %s', (product_id,))
+    cursor.execute('SELECT * FROM products WHERE products = %s', (product_id,))
     current_product_info = cursor.fetchone()
     return render_template('update_product.html', current_product_info=current_product_info)
 
@@ -58,7 +58,7 @@ def delete_product(product_id):
     cursor = db.cursor()
 
     # Delete the product
-    cursor.execute('DELETE FROM product_info WHERE product_id = %s', (product_id,))
+    cursor.execute('DELETE FROM products WHERE product_id = %s', (product_id,))
     db.commit()
 
     flash('Product deleted successfully!', 'danger')
